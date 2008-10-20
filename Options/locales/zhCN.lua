bb7311066adb8bebaededb0d8ed78c5e3453693b
ananhaid <ananhaid@98ddf5e5-c516-4155-a43e-68834e374b74>
2008-10-20 11:27:41 +0000

    Grid2: - add zhCN locale.

---
+++ Options/locales/zhCN.lua
@@ -0,0 +1,95 @@
+﻿local L =  LibStub:GetLibrary("AceLocale-3.0"):NewLocale("Grid2Options", "zhCN")
+if not L then return end
+
+L["Debug"] = "除错"
+L["Debugging"] = "除错"
+L["Module debugging menu."] = "除错模块设置。"
+
+L["Show Tooltip"] = "显示提示信息"
+L["Show unit tooltip.  Choose 'Always', 'Never', or 'OOC'."] = "显示单位框架的鼠标信息。选择“一直”，“不显示”或“非战斗”。"
+L["Always"] = "一直"
+L["Never"] = "不显示"
+L["OOC"] = "非战斗"
+
+L["Advanced"] = "高级"
+L["Advanced options."] = "高级选项。"
+
+L["Frame Width"] = "框架宽度"
+L["Adjust the width of each unit's frame."] = "调整个体框架的宽度。"
+L["Frame Height"] = "框架高度"
+L["Adjust the height of each unit's frame."] = "调整个体框架的高度。"
+
+L["Options for %s."] = "%s状态的选项。"
+L["Toggle debugging for %s."] = "激活%s的除错。"
+
+L["Show Frame"] = "显示框架"
+L["Sets when the Grid is visible: Choose 'Always', 'Grouped', or 'Raid'."] = "选择什么时候显示Grid：“一直”，“组队”或“团队”。"
+L["Always"] = "一直"
+L["Grouped"] = "组队"
+L["Raid"] = "团队"
+
+L["Raid Layout"] = "团队布局"
+L["Select which layout to use for raid."] = "选择使用哪个团队布局。"
+
+L["Horizontal groups"] = "横向排列队伍"
+L["Switch between horzontal/vertical groups."] = "选择横向/竖向显示队伍。"
+L["Clamped to screen"] = "保持在屏幕上"
+L["Toggle whether to permit movement out of screen."] = "打开/关闭是否允许把窗口移到超出屏幕的位置。"
+L["Frame lock"] = "锁定框架"
+L["Locks/unlocks the grid for movement."] = "锁定/解锁 Grid 框体来让其移动。"
+L["Click through the Grid Frame"] = "透过 Grid 框体点击"
+L["Allows mouse click through the Grid Frame."] = "是否允许透过 Grid 框体点击。"
+
+L["Padding"] = "填白"
+L["Adjust frame padding."] = "调整框架填白。"
+L["Spacing"] = "空隙"
+L["Adjust frame spacing."] = "调整框架空隙。"
+L["Scale"] = "大小比率"
+L["Adjust Grid scale."] = "调整框架大小比率。"
+
+L["Border"] = "边缘"
+L["Adjust border color and alpha."] = "调整边缘的颜色和透明度。"
+L["Background"] = "背景"
+L["Adjust background color and alpha."] = "调整背景颜色和透明度。"
+
+L["Layout Anchor"] = "布局锚点"
+L["Sets where Grid is anchored relative to the screen."] = "设置屏幕中 Grid 的锚点。"
+
+L["CENTER"] = "中心"
+L["TOP"] = "顶部"
+L["BOTTOM"] = "底部"
+L["LEFT"] = "左侧"
+L["RIGHT"] = "右侧"
+L["TOPLEFT"] = "左上"
+L["TOPRIGHT"] = "右上"
+L["BOTTOMLEFT"] = "左下"
+L["BOTTOMRIGHT"] = "右下"
+
+L["Group Anchor"] = "队伍锚点"
+L["Sets where groups are anchored relative to the layout frame."] = "设置布局中队伍的锚点。"
+L["Reset Position"] = "重置位置"
+L["Resets the layout frame's position and anchor."] = "重置布局框架的位置和锚。"
+
+L["Center Text Length"] = "中间文字长度"
+L["Number of characters to show on Center Text indicator."] = "中间文字提示器所显示文字的长度。"
+L["Font Size"] = "字体大小"
+L["Adjust the font size."] = "调整字体尺寸。"
+L["Font"] = "字体"
+L["Adjust the font settings"] = "调整字体"
+L["Frame Texture"] = "框架材质"
+L["Adjust the texture of each unit's frame."] = "调整个体框架的材质。"
+L["Orientation of Frame"] = "框架方向"
+L["Set frame orientation."] = "设置框架方向。"
+L["VERTICAL"] = "竖直"
+L["HORIZONTAL"] = "水平"
+
+L["Icon Size"] = "图标大小"
+L["Adjust the size of the center icon."] = "调整中心图标的尺寸。"
+L["Corner Size"] = "边角指示大小"
+L["Adjust the size of the corner indicators."] = "调整边角指示的大小。"
+
+
+L["Color"] = "颜色"
+L["Color for %s."] = "%s的颜色。"
+L["Invert Bar Color"] = "反转颜色"
+L["Swap foreground/background colors on bars."] = "反转提示条上背景/前景的颜色。"
