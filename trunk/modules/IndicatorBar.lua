ffc6a232b18442a3a43060dba0bdcdef0236e397
jerry <jerry@98ddf5e5-c516-4155-a43e-68834e374b74>
2008-10-27 09:15:33 +0000

    Fix Bar Indicator Generator. Fix Default Configuration.

--- trunk/modules/IndicatorBar.lua
+++ trunk/modules/IndicatorBar.lua
@@ -4,50 +4,55 @@ local function Bar_Create(self, parent)
 	local media = LibStub("LibSharedMedia-3.0", true)
 	local texture = media and media:Fetch("statusbar", self.db.profile.texture) or "Interface\\Addons\\Grid2\\gradient32x32"
 
-	parent.BarBG = parent:CreateTexture()
-	parent.BarBG:SetTexture(texture)
+
+	local BarBG = parent:CreateTexture()
+	parent[self.nameBG] = BarBG
+	BarBG:SetTexture(texture)
 
 	-- create bar
-	parent.Bar = CreateFrame("StatusBar", nil, parent)
-	parent.Bar:SetStatusBarTexture(texture)
-	parent.Bar:SetMinMaxValues(0, 1)
-	parent.Bar:SetValue(1)
-	parent.Bar:SetStatusBarColor(0,0,0,0.8)
+	local Bar = CreateFrame("StatusBar", nil, parent)
+	parent[self.name] = Bar
+	Bar:SetStatusBarTexture(texture)
+	Bar:SetMinMaxValues(0, 1)
+	Bar:SetValue(1)
+	Bar:SetStatusBarColor(0,0,0,0.8)
 
-	parent.BarBG:SetPoint(self.anchor, parent, self.anchorRel, self.offsetx, self.offsety)
-	parent.Bar:SetPoint(self.anchor, parent, self.anchorRel, self.offsetx, self.offsety)
+	BarBG:SetPoint(self.anchor, parent, self.anchorRel, self.offsetx, self.offsety)
+	Bar:SetPoint(self.anchor, parent, self.anchorRel, self.offsetx, self.offsety)
 	self:SetOrientation(parent)
 end
 
 local function Bar_Layout(self, parent)
 	local inset = self.db.profile.inset or 2
 	local w, h = parent:GetWidth() - inset, parent:GetHeight() - inset
-	parent.BarBG:SetWidth(w)
-	parent.BarBG:SetHeight(h)
-	parent.Bar:SetWidth(w)
-	parent.Bar:SetHeight(h)
+	local Bar, BarBG = parent[self.name], parent[self.nameBG]
+	BarBG:SetWidth(w)
+	BarBG:SetHeight(h)
+	Bar:SetWidth(w)
+	Bar:SetHeight(h)
 end
 
 local function Bar_GetBlinkFrame(self, parent)
-	return parent.Bar
+	return parent[self.name]
 end
 
 local function Bar_OnUpdate(self, parent, unit, status)
+	local Bar = parent[self.name]
 	if status then
-		parent.Bar:SetValue(status:GetPercent(unit))
+		Bar:SetValue(status:GetPercent(unit))
 	else
-		parent.Bar:SetValue(1)
+		Bar:SetValue(1)
 	end
 end
 
 local function Bar_SetOrientation(self, parent, orientation)
 	orientation = orientation or self.db.profile.orientation
-	parent.Bar:SetOrientation(orientation)
+	parent[self.name]:SetOrientation(orientation)
 end
 
 local function Bar_SetTexture(self, parent, texture)
-	parent.BarBG:SetTexture(texture)
-	parent.Bar:SetStatusBarTexture(texture)
+	parent[self.nameBG]:SetTexture(texture)
+	parent[self.name]:SetStatusBarTexture(texture)
 end
 
 local Bar_defaultDB = {
@@ -73,12 +78,13 @@ local function BarColor_OnUpdate(self, parent, unit, status)
 end
 
 local function BarColor_SetBarColor(self, parent, r, g, b, a)
+	local Bar, BarBG = parent[self.name], parent[self.nameBG]
 	if self.db.profile.invertBarColor then
-		parent.Bar:SetStatusBarColor(r, g, b, a)
-		parent.BarBG:SetVertexColor(0, 0, 0, 0)
+		Bar:SetStatusBarColor(r, g, b, a)
+		BarBG:SetVertexColor(0, 0, 0, 0)
 	else
-		parent.Bar:SetStatusBarColor(0, 0, 0, 0.8)
-		parent.BarBG:SetVertexColor(r, g, b ,a)
+		Bar:SetStatusBarColor(0, 0, 0, 0.8)
+		BarBG:SetVertexColor(r, g, b ,a)
 	end
 end
 
@@ -92,6 +98,8 @@ function Grid2:CreateBarIndicator(name, anchor, anchorRel, offsetx, offsety)
 	name = "bar-"..name
 
 	local Bar = self.indicatorPrototype:new(name)
+	Bar.name = name
+	Bar.nameBG = name.."-background"
 	Bar.anchor = anchor
 	Bar.anchorRel = anchorRel
 	Bar.offsetx = offsetx
@@ -107,6 +115,8 @@ function Grid2:CreateBarIndicator(name, anchor, anchorRel, offsetx, offsety)
 	self:RegisterIndicator(Bar, { "percent" })
 
 	local BarColor = self.indicatorPrototype:new(name.."-color")
+	BarColor.name = Bar.name
+	BarColor.nameBG = Bar.nameBG
 	BarColor.Create = BarColor_Create
 	BarColor.Layout = BarColor_Layout
 	BarColor.OnUpdate = BarColor_OnUpdate
