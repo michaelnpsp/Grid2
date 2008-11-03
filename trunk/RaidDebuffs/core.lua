7c90ad76b8faeb1c2c5bbdfe5d92d66832ec34e9
jerry <jerry@98ddf5e5-c516-4155-a43e-68834e374b74>
2008-11-03 10:53:22 +0000

    Remove test data. Reuse the spell table.

--- trunk/RaidDebuffs/core.lua
+++ trunk/RaidDebuffs/core.lua
@@ -8,17 +8,15 @@ local spellDB = {
 	[BZ["Hyjal Summit"]] = { 31249, 31306, 31347, 31341, 31344, 31944, 31972, },
 	[BZ["Black Temple"]] = { 34654, 39674, 41150, 41168, 39837, 40239, 40251, 40604, 40481, 40508, 42005, 41303, 41410, 41376, 40860, 41001, 41485, 41472, 41914, 41917, 40585, 40932, },
 	[BZ["Sunwell Plateau"]] = { 46561, 46562, 46266, 46557, 46560, 46543, 46427, 45032, 45034, 45018, 46384, 45150, 45855, 45662, 45402, 45717, 45256,  45333, 46771, 45270, 45347, 45348, 45996, 45442, 45641, 45885, 45737, 45740, 45741, },
-	["Forêt du Chant de cristal"] = { 37328, 58624 },
 }
 
 local GSRD = Grid2:NewModule("StatusRaidDebuffs")
 local status = Grid2.statusPrototype:new("raid-debuffs")
 local frame = CreateFrame"Frame"
-local defaultDB = {}
-local spells
+local spells = {}
 
 function GSRD:UpdateZoneSpells(zone)
-	spells = {}
+	wipe(spells)
 	local zone = zone or GetRealZoneText()
 	local db = spellDB[zone]
 	if db then
