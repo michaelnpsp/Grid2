d83ff21d50000b10e156eb5600e6c22d58b8d875
jerry <jerry@98ddf5e5-c516-4155-a43e-68834e374b74>
2008-12-23 20:59:04 +0000

    start to add setup support to options

---
+++ Options/GridDefaults.lua
@@ -0,0 +1,8 @@
+
+-- Auras
+for name, data in pairs(Grid2.db.profile.setup.buffs) do
+	Grid2Options:AddAura("Buff", name, unpack(data))
+end
+for name, data in pairs(Grid2.db.profile.setup.debuffs) do
+	Grid2Options:AddAura("Debuff", name, unpack(data))
+end
