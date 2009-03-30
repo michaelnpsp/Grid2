61fc55155406d91d415a965d369d08dae2058303
Azethoth <Azethoth@98ddf5e5-c516-4155-a43e-68834e374b74>
2009-03-30 23:23:28 +0000

    --Options for Raid Debuffs

---
+++ trunk/RaidDebuffsOptions/locales/koKR.lua
@@ -0,0 +1,4 @@
+﻿local L =  LibStub:GetLibrary("AceLocale-3.0"):NewLocale("Grid2Options", "koKR")
+if not L then return end
+
+L["group"] = "group"
---
+++ trunk/RaidDebuffsOptions/locales/ruRU.lua
@@ -0,0 +1,4 @@
+﻿local L =  LibStub:GetLibrary("AceLocale-3.0"):NewLocale("Grid2Options", "ruRU")
+if not L then return end
+
+L["group"] = "group"
