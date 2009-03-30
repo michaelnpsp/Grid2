402b2afcc5dabac32d99d0ade21ed26acc5736a1
Azethoth <Azethoth@98ddf5e5-c516-4155-a43e-68834e374b74>
2009-03-30 23:25:39 +0000

    --defaults

--- trunk/RaidDebuffs/core.lua
+++ trunk/RaidDebuffs/core.lua
@@ -8,11 +8,12 @@ local spellDB = {
 	[BZ["Hyjal Summit"]] = { 31249, 31306, 31347, 31341, 31344, 31944, 31972, },
 	[BZ["Black Temple"]] = { 34654, 39674, 41150, 41168, 39837, 40239, 40251, 40604, 40481, 40508, 42005, 41303, 41410, 41376, 40860, 41001, 41485, 41472, 41914, 41917, 40585, 40932, },
 	[BZ["Sunwell Plateau"]] = { 46561, 46562, 46266, 46557, 46560, 46543, 46427, 45032, 45034, 45018, 46384, 45150, 45855, 45662, 45402, 45717, 45256,  45333, 46771, 45270, 45347, 45348, 45996, 45442, 45641, 45885, 45737, 45740, 45741, },
+	[BZ["Ulduar"]] = { 62589, 63571, 62532, 62589, 61969, 61990, 62548, 63476, 62680, 63472, 62717, 63477, 64290, 64292, 62042, 62130, 63024, 64234, 63018 },
 }
 
 local GSRD = Grid2:NewModule("StatusRaidDebuffs")
 local status = Grid2.statusPrototype:new("raid-debuffs")
-local frame = CreateFrame"Frame"
+local frame = CreateFrame("Frame")
 local spells = {}
 
 function GSRD:UpdateZoneSpells(zone)
@@ -37,19 +38,19 @@ function GSRD:UpdateZoneSpells(zone)
 		end
 	end
 	if #spells == 0 then
-		frame:UnregisterEvent"UNIT_AURA"
+		frame:UnregisterEvent("UNIT_AURA")
 	else
-		frame:RegisterEvent"UNIT_AURA"
+		frame:RegisterEvent("UNIT_AURA")
 	end
 end
 
 function status:OnEnable()
-	frame:RegisterEvent"ZONE_CHANGED_NEW_AREA"
+	frame:RegisterEvent("ZONE_CHANGED_NEW_AREA")
 	GSRD:UpdateZoneSpells()
 end
 
 function status:OnDisable()
-	frame:UnregisterEvent"ZONE_CHANGED_NEW_AREA"
+	frame:UnregisterEvent("ZONE_CHANGED_NEW_AREA")
 end
 
 local states = {}
@@ -144,8 +145,12 @@ end)
 
 Grid2:RegisterStatus(status, { "icon" })
 
-local prev_SetupIndicators = Grid2.SetupIndicators
-function Grid2:SetupIndicators(...)
-	prev_SetupIndicators(self, ...)
-	self.indicators["icon-center"]:RegisterStatus(self.statuses["raid-debuffs"], 1000)
+local prev_MakeDefaultSetup = Grid2.MakeDefaultSetup
+function Grid2:MakeDefaultSetup(setup, class, ...)
+	prev_MakeDefaultSetup(self, setup, class, ...)
+
+	local setupIndicator = setup.status
+	self:SetupIndicatorStatus(setupIndicator, "icon-center", "raid-debuffs", 1000)
+
+--	self.indicators["icon-center"]:RegisterStatus(self.statuses["raid-debuffs"], 1000)
 end
