8ddf6b12f4262a9f2bc55e28edd7aa4d73d95a5b
Azethoth <Azethoth@98ddf5e5-c516-4155-a43e-68834e374b74>
2009-05-20 10:28:35 +0000

    --Increase colors for Grace / Prayer of Mending to 3 / 5 each --More random vehicle million monkey coding

--- modules/StatusName.lua
+++ modules/StatusName.lua
@@ -2,14 +2,26 @@ local Name = Grid2.statusPrototype:new("name")
 
 function Name:OnEnable()
 	self:RegisterEvent("UNIT_NAME_UPDATE")
+	self:RegisterEvent("Grid_UnitJoined")
+	self:RegisterEvent("Grid_UnitChanged")
 end
 
-function Name:UNIT_NAME_UPDATE(_, unit)
-	return self:UpdateIndicators(unit)
+function Name:UNIT_NAME_UPDATE(_, unitid)
+	return self:UpdateIndicators(unitid)
+end
+
+function Name:Grid_UnitJoined(_, unitid, guid)
+	return self:UpdateIndicators(unitid)
+end
+
+function Name:Grid_UnitChanged(_, unitid, guid)
+	return self:UpdateIndicators(unitid)
 end
 
 function Name:OnDisable()
 	self:UnregisterEvent("UNIT_NAME_UPDATE")
+	self:UnregisterEvent("Grid_UnitJoined")
+	self:UnregisterEvent("Grid_UnitChanged")
 end
 
 function Name:IsActive(unit)
