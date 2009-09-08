ce4ee98c86a3e2388c3cc50adcce31bcf582347d
Azethoth <Azethoth@98ddf5e5-c516-4155-a43e-68834e374b74>
2009-09-08 20:05:11 +0000

    Missing wotlk instances More commenting of ids Deleted some obsolete ids

--- trunk/RaidDebuffs/Grid2StatusRaidDebuffs.lua
+++ trunk/RaidDebuffs/Grid2StatusRaidDebuffs.lua
@@ -8,8 +8,164 @@ local spellDB = {
 	[BZ["Hyjal Summit"]] = { 31249, 31306, 31347, 31341, 31344, 31944, 31972, },
 	[BZ["Black Temple"]] = { 34654, 39674, 41150, 41168, 39837, 40239, 40251, 40604, 40481, 40508, 42005, 41303, 41410, 41376, 40860, 41001, 41485, 41472, 41914, 41917, 40585, 40932, },
 	[BZ["Sunwell Plateau"]] = { 46561, 46562, 46266, 46557, 46560, 46543, 46427, 45032, 45034, 45018, 46384, 45150, 45855, 45662, 45402, 45717, 45256, 45333, 46771, 45270, 45347, 45348, 45996, 45442, 45641, 45885, 45737, 45740, 45741, },
+	[BZ["Naxxramas"]] = {
+		--Trash	
+		55314,--Strangulate
+
+		--Anub'Rekhan
+		28786, 54022,--Locust Swarm (N, H)
+
+		--Grand Widow Faerlina
+		28796, 54098,--Poison Bolt Volley (N, H)
+		28794, 54099,--Rain of Fire (N, H)
+
+		--Maexxna
+		28622,--Web Wrap (NH)
+		54121, 28776,--Necrotic Poison (N, H)
+
+		--Noth the Plaguebringer
+		29213, 54835,--Curse of the Plaguebringer (N, H)
+		29214, 54836,--Wrath of the Plaguebringer (N, H)
+		29212,--Cripple (NH)
+
+		--Heigan the Unclean
+		29998, 55011,--Decrepit Fever (N, H)
+		29310,--Spell Disruption (NH)
+
+		--Grobbulus
+		28169,--Mutating Injection (NH)
+
+		--Gluth
+		54378,--Mortal Wound (NH)			
+		29306,--Infected Wound (NH)
+
+		--Thaddius
+		28084, 28085,--Negative Charge (N, H)
+		28059, 28062,--Positive Charge (N, H)
+
+		--Instructor Razuvious
+		55550,--Jagged Knife (NH)
+					
+		--Sapphiron
+		28522,--Icebolt (NH)
+		28542, 55665,--Life Drain (N, H)
+
+		--Kel'Thuzad
+		28410,--Chains of Kel'Thuzad (H)
+		27819,--Detonate Mana (NH)
+		27808,--Frost Blast (NH)
+	},
+	[BZ["The Eye of Eternity"]] = {
+		--Malygos	
+		56272, 60072,--Arcane Breath (N, H)
+		57407, 60936,--Surge of Power (N, H)
+	},
+	[BZ["The Obsidian Sanctum"]] = {
+		--Trash	
+		39647,--Curse of Mending
+		58936,--Rain of Fire
+
+		--Sartharion	
+		60708,--Fade Armor (N, H)
+		57491,--Flame Tsunami (N, H)
+	},
+	[BZ["Trial of the Crusader"]] = {
+		--Gormok the Impaler
+		66331, 67477, 67478, 67479,--Impale(10, 25, 10H, 25H)
+		66406,--Snobolled!
+
+		--Acidmaw --Dreadscale
+		66819, 67609, 67610, 67611,--Acidic Spew (10, 25, 10H, 25H)
+		66821, 67635, 67636, 67637,--Molten Spew (10, 25, 10H, 25H)
+		66823, 67618, 67619, 67620,--Paralytic Toxin (10, 25, 10H, 25H)
+		66869,--Burning Bile
+
+		--Icehowl
+		66770, 67654, 67655, 67656,--Ferocious Butt(10, 25, 10H, 25H)
+		66689, 67650, 67651, 67652,--Arctic Breathe(10, 25, 10H, 25H)
+		66683,--Massive Crash
+
+		--Lord Jaraxxus
+		66532, 66963, 66964, 66965,--Fel Fireball (10, 25, 10H, 25H)
+		66237, 67049, 67050, 67051,--Incinerate Flesh (10, 25, 10H, 25H)
+		66242, 67059, 67060, 67061,--Burning Inferno (10, 25, 10H, 25H)
+		66197, 68123, 68124, 68125,--Legion Flame (10, 25, 10H, 25H)
+		66199, 68126, 68127, 68128,--Legion Flame (Patch?: 10, 25, 10H, 25H)
+		66877, 67070, 67071, 67072,--Legion Flame (Patch Icon?: 10, 25, 10H, 25H)
+		66283,--Spinning Pain Spike
+		66209,--Touch of Jaraxxus(H)
+		66211,--Curse of the Nether(H)
+		66333, 66334, 66335, 66336, 68156,--Mistress' Kiss (10H, 25H)
+
+		--Faction Champions
+		65812, 68154, 68155, 68156,--Unstable Affliction (10, 25, 10H, 25H)
+		--65960,--Blind
+		--65801,--Polymorph
+		--65543,--Psychic Scream
+		--66054,--Hex
+		--65809,--Fear
+
+		--The Twin Val'kyr
+		67176,--Dark Essence
+		67223,--Light Essence
+		67282, 67283,--Dark Touch
+		67297, 67298,--Light Touch
+		67309, 67310, 67311, 67312,--Twin Spike (10, 25, 10H, 25H)
+
+		--Anub'arak
+		67574,--Pursued by Anub'arak
+		--66240, 67630, 68646, 68647,--Leeching Swarm (10, 25, 10H, 25H)
+		66013, 67700, 68509, 68510,--Penetrating Cold (10, 25, 10H, 25H)
+		67847, 67721,--Expose Weakness
+		66012,--Freezing Slash
+	},
 	[BZ["Ulduar"]] = {
-		62928, 63612, 63673, 63615, 63830, 64705, 64706, 62589, 63571, 62532, 62861, 62930, 62469, 61969, 61990, 62188, 62548, 63476, 62680, 63472, 62717, 63477, 64290, 64292, 63355, 64002, 62055, 62042, 62130, 62526, 62470, 62331, 62418, 63024, 64234, 63018, 65121, 61888, 64637, 62269, 63490, 61903, 63493,
+		--Trash
+		62310, 62928,--Impale (N, H)
+		63612, 63673,--Lightning Brand (N, H)
+		63615,--Ravage Armor (NH)
+		62283, 62438,--Iron Roots (N, H)
+		63169, 63549,--Petrify Joints (N, H)
+					
+		--Razorscale
+		64771,--Fuse Armor (NH)
+					
+		--Ignis the Furnace Master
+		62548, 63476,--Scorch (N, H)
+		62680, 63472,--Flame Jet (N, H)
+		62717, 63477,--Slag Pot (N, H)
+					
+		--XT-002
+		63024, 64234,--Gravity Bomb (N, H)
+		63018, 65121,--Light Bomb (N, H)
+					
+		--The Assembly of Iron
+		61888, 64637,--Overwhelming Power (N, H)
+		62269, 63490,--Rune of Death (N, H)
+		61903, 63493,--Fusion Punch (N, H)
+		61912, 63494,--Static Disruption(N, H)
+					
+		--Kologarn
+		64290, 64292,--Stone Grip (N, H)
+		63355, 64002,--Crunch Armor (N, H)
+		62055,--Brittle Skin (NH)			
+			
+		--Hodir
+		62469,--Freeze (NH)
+		61969, 61990,--Flash Freeze (N, H)
+		62188,--Biting Cold (NH)
+					
+		--Thorim
+		62042,--Stormhammer (NH)				
+		62130,--Unbalancing Strike (NH)
+		62526,--Rune Detonation (NH)
+		62470,--Deafening Thunder (NH)
+		62331, 62418,--Impale (N, H)
+			
+		--Freya
+		62532,--Conservator's Grip (NH)
+		62589, 63571,--Nature's Fury (N, H)
+		62861, 62930,--Iron Roots (N, H)
 
 		--Mimiron
 		63666,--Napalm Shell (N)
@@ -23,8 +179,9 @@ local spellDB = {
 		63322,--Saronite Vapors (NH)
 
 		--Yogg-Saron
-		63134,--Sara's Bless(NH)
-		63138,--Sara's Fevor(NH)
+		63147,--Sara's Anger(NH)
+		63134,--Sara's Blessing(NH)
+		63138,--Sara's Fervor(NH)
 		63830,--Malady of the Mind (H)
 		63802,--Brain Link(H)
 		63042,--Dominate Mind (H)
@@ -38,51 +195,9 @@ local spellDB = {
 		--Algalon
 		64412,--Phase Punch
 	},
-	[BZ["Trial of the Crusader"]] = {
-		--Gormok the Impaler
-		66331, 67477, 67478, 67479,--Impale(10, 25, 10H, 25H)
-		66406,--Snobolled!
-
-		--Acidmaw --Dreadscale
-		66819, 67609, 67610, 67611,--Acidic Spew(10, 25, 10H, 25H)
-		66821, 67635, 67636, 67637,--Molten Spew(10, 25, 10H, 25H)
-		66823, 67618, 67619, 67620,--Paralytic Toxin(10, 25, 10H, 25H)
-		66869,--Burning Bile
-
-		--Icehowl
-		66770, 67654, 67655, 67656,--Ferocious Butt(10, 25, 10H, 25H)
-		66689, 67650, 67651, 67652,--Arctic Breathe(10, 25, 10H, 25H)
-		66683,--Massive Crash
-
-		--Lord Jaraxxus
-		66532, 66963, 66964, 66965,--Fel Fireball (10, 25, 10H, 25H)
-		66237, 67049, 67050, 67051,--Incinerate Flesh(10, 25, 10H, 25H)
-		66242, 67059, 67060, 67061,--Burning Inferno (10, 25, 10H, 25H)
-		66197, 68123, 68124, 68125,--Legion Flame (10, 25, 10H, 25H)
-		66199, 68126, 68127, 68128,--Legion Flame (Patch?: 10, 25, 10H, 25H)
-		66877, 67070, 67071, 67072,--Legion Flame (Patch Icon?: 10, 25, 10H, 25H)
-		66283,--Spinning Pain Spike
-		66209,--Touch of Jaraxxus(H)
-		66211,--Curse of the Nether(H)
-
-		--Faction Champions
-		65812, 68154, 68155, 68156,--Unstable Affliction(10, 25, 10H, 25H)
-		--65960,--Blind
-		--65801,--Polymorph
-		--65543,--Psychic Scream
-		--66054,--Hex
-		--65809,--Fear
-
-		--The Twin Val'kyr
-		67176,--Dark Essence
-		67223,--Light Essence
-		67283,--Dark Touch
-		67298,--Light Touch
-
-		--Anub'arak
-		67574,--Pursued by Anub'arak
-		--66240, 67630, 68646, 68647,--Leeching Swarm (10, 25, 10H, 25H)
-		66013, 67700, 68509, 68510,--Penetrating Cold (10, 25, 10H, 25H)
+	[BZ["Vault of Archavon"]] = {
+		--Koralon	
+		67332, 66684,--Flaming Cinder (10, 25)
 	},
 }
 
