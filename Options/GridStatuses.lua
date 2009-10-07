9f7a4c6a464febcd945874abb82abbb5ff8f0dfb
jerry <jerry@98ddf5e5-c516-4155-a43e-68834e374b74>
2009-10-07 09:46:48 +0000

    Fix Range option list.

--- Options/GridStatuses.lua
+++ Options/GridStatuses.lua
@@ -690,7 +690,7 @@ function Grid2Options:AddSetupStatusesOptions(setup, reset)
 			values = {
 				[10] = (L["%d yards"]:format(10)),
 				[28] = (L["%d yards"]:format(28)),
-				[38] = (L["%d yards"]:format(38)),
+				[40] = (L["%d yards"]:format(40)),
 				[100] = (L["%d yards"]:format(100)),
 			},
 		},
