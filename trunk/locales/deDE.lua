cde215936f1b673743f09a97b8ae174cfe56dd3f
jerry <jerry@98ddf5e5-c516-4155-a43e-68834e374b74>
2010-01-19 07:30:24 +0000

    Update deDE. Thanks Vilogity

--- trunk/locales/deDE.lua
+++ trunk/locales/deDE.lua
@@ -2,7 +2,7 @@
 if not L then return end
 
 --{{{ Actually used
-L["Border"] = "Umrandung"
+L["Border"] = "Rand"
 L["Charmed"] = "Verzaubert"
 L["Drink"] = "Trinken"
 L["Food"] = "Essen"
@@ -17,12 +17,12 @@ L["Configure Grid"] = "Konfiguration Grid"
 --}}}
 
 --{{{ GridFrame
-L["Frame"] = "Frame"
-L["Options for GridFrame."] = "Optionen für Grid Frame"
+L["Frame"] = "Rahmen"
+L["Options for GridFrame."] = "Optionen für Grid Rahmen"
 
 L["Indicators"] = "Indikatoren"
-L["Health Bar"] = "Heal Leiste"
-L["Health Bar Color"] = "Heal Leiste Farbe"
+L["Health Bar"] = "Gesundheitsleiste"
+L["Health Bar Color"] = "Gesundheitsleisten Farbe"
 L["Center Text"] = "Center Text"
 L["Center Text 2"] = "Center Text 2"
 L["Center Icon"] = "Center Icon"
@@ -33,14 +33,14 @@ L["Bottom Right Corner"] = "Ecke unten Rechts"
 L["Frame Alpha"] = "Frame Alpha"
 
 L["Options for %s indicator."] = "Optionen für %s Indikator"
-L["Statuses"] = "Status"
-L["Toggle status display."] = "Umschalter Status Display"
+L["Statuses"] = "Zustand"
+L["Toggle status display."] = "einschalten/ausschalten Zustands Display"
 
 -- Advanced options
 L["Enable %s indicator"] = "Aktiviert %s indikator"
 L["Toggle the %s indicator."] = "Schaltet den %s um."
 L["Orientation of Text"] = "Ausrichtung des Textes"
-L["Set frame text orientation."] = "Setze Frame Text Ausrichtung"
+L["Set frame text orientation."] = "Setze Rahmen Text Ausrichtung"
 --}}}
 
 --{{{ GridLayout
@@ -50,18 +50,18 @@ L["Options for GridLayout."] = "Optionen für GridLayout"
 -- Layout options
 L["Raid Layout"] = "Raid Layout"
 L["Select which raid layout to use."] = "Auswahl welches Raid Layout genutzt werden soll"
-L["Show Party in Raid"] = "Zeige Party im Raid"
-L["Show party/self as an extra group."] = "Zeige Party/selbst als eine extra Gruppe"
-L["Show Pets for Party"] = "Zeige die Pets bei Party"
-L["Show the pets for the party below the party itself."] = " Zeige Pets der Party unter dem Party Fenster"
+L["Show Party in Raid"] = "Zeige deine Gruppe im Raid"
+L["Show party/self as an extra group."] = "Zeige 5er Gruppe/selbst als eine extra Gruppe"
+L["Show Pets for Party"] = "Zeige die Pets in einer 5er Gruppe"
+L["Show the pets for the party below the party itself."] = " Zeige Pets der 5er Gruppe unter dem Gruppen Fenster"
 
 -- Display options
 L["Pet color"] = "Pet Farbe"
 L["Set the color of pet units."] = "Setzt eine Farbe für die Begleiter Einheiten"
 L["Pet coloring"] = "Begleiter Farbe"
-L["Set the coloring strategy of pet units."] = "Set the coloring strategy of pet units."
-L["By Owner Class"] = "Durch Besitzer Klasse"
-L["By Creature Type"] = "Durch Kreatur Art"
+L["Set the coloring strategy of pet units."] = "Legt fest, wie die Begleiter eingefÃ¤rbt werden."
+L["By Owner Class"] = "Nach Besitzer Klasse"
+L["By Creature Type"] = "Nach Kreatur Art"
 L["Using Fallback color"] = "Benutze Rücksicherungs Farbe"
 L["Beast"] = "Bestie"
 L["Demon"] = "Dämon"
@@ -89,27 +89,27 @@ L["Advanced options."] = "Erweiterte Optionen"
 --{{{ GridLayoutLayouts
 -- ToDo: move into options
 L["None"] = "Keine"
-L["Solo"] = "Alleine"
+L["Solo"] = "Allein"
 L["Solo w/Pet"] = "Allein mit Begleiter"
-L["By Group 5"] = "Bei Gruppe5"
-L["By Group 5 w/Pets"] = "Bei Gruppe 5 mit Begleiter"
-L["By Group 40"] = "Bei Gruppe 40"
-L["By Group 25"] = "Bei Gruppe 25"
-L["By Group 25 w/Pets"] = "Bei Gruppe 25 mit Begleiter"
-L["By Group 20"] = "Bei Gruppe 20"
-L["By Group 15"] = "Bei Gruppe 15"
-L["By Group 15 w/Pets"] = "Bei Gruppe 15 mit Begleiter"
-L["By Group 10"] = "Bei Gruppe 10"
-L["By Group 10 w/Pets"] = "Bei Gruppe 10 mit Begleiter"
-L["By Group 4 x 10 Wide"] = "Bei Gruppe 4 x 10 Breit"
-L["By Class 25"] = "Bei Klasse 25"
-L["By Class 1 x 25 Wide"] = "Bei Klasse 1 x 25 Breit"
-L["By Class 2 x 15 Wide"] = "Bei Klasse 2x 15 Breit"
-L["By Role 25"] = "Bei Rolle 25"
-L["By Class"] = "Bei Klasse"
-L["By Class w/Pets"] = "Bei Klasse mit Pets"
+L["By Group 5"] = "5er Gruppe"
+L["By Group 5 w/Pets"] = "5er Gruppe mit Begleiter"
+L["By Group 40"] = "40er Gruppe"
+L["By Group 25"] = "25er Gruppe"
+L["By Group 25 w/Pets"] = "25er Gruppe mit Begleiter"
+L["By Group 20"] = "20er Gruppe"
+L["By Group 15"] = "15er Gruppe"
+L["By Group 15 w/Pets"] = "15 Gruppe mit Begleiter"
+L["By Group 10"] = "10er Gruppe"
+L["By Group 10 w/Pets"] = "10er Gruppe mit Begleiter"
+L["By Group 4 x 10 Wide"] = "Gruppe 4 x 10 Breit"
+L["By Class 25"] = "25er nach Klasse"
+L["By Class 1 x 25 Wide"] = "Nach Klasse 1 x 25 Breit"
+L["By Class 2 x 15 Wide"] = "Nach Klasse 2x 15 Breit"
+L["By Role 25"] = "25er nach Rolle"
+L["By Class"] = "Nach Klasse"
+L["By Class w/Pets"] = "Nach Klasse mit Pets"
 L["Onyxia"] = "Onyxia"
-L["By Group 25 w/tanks"] = "Bei Gruppe 25 mit Tanks"
+L["By Group 25 w/tanks"] = "25er Gruppe mit Tanks"
 --}}}
 
 --{{{ GridRange
@@ -146,7 +146,7 @@ L["Disease"] = "Krankheit"
 L["Magic"] = "Magie"
 L["Curse"] = "Fluch"
 L["Ghost"] = "Geist"
-L["Add new Buff"] = "Add neuen Buff"
+L["Add new Buff"] = "Neuen Buff zufügen"
 L["Adds a new buff to the status module"] = "Fügt einen neuen Buff zum Status Module"
 L["<buff name>"] = "<Buff Name>"
 L["Add new Debuff"] = "Fügt einen neuen Debuff hinzu"
@@ -167,14 +167,14 @@ L["Skip units that have an active Abolish buff."] = "Überspringe Einheiten mit 
 --}}}
 
 --{{{ GridStatusName
-L["Unit Name"] = "Unit Name"
+L["Unit Name"] = "Einheiten Name"
 L["Color by class"] = "Farbe bei Klasse"
 --}}}
 
 --{{{ GridStatusMana
 L["Mana"] = "Mana"
 L["Low Mana"] = "Wenig Mana"
-L["Mana threshold"] = "Mana Schwelle"
+L["Mana threshold"] = "Mana Grenzwert"
 L["Set the percentage for the low mana warning."] = "Setze die Prozente für die wenig Mana Warnung"
 L["Low Mana warning"] = "Wenig Mana Warnung"
 --}}}
@@ -205,12 +205,12 @@ L["Health"] = "Gesundheit"
 L["Show dead as full health"] = "Zeige Tote mit voller Gesundheit"
 L["Treat dead units as being full health."] = "Behandle Tote Einheiten als hätten sie volle Gesundheit."
 L["Use class color"] = "Benutze Klassen Farbe"
-L["Color health based on class."] = "HP Farbe basierend auf Klasse"
-L["Health threshold"] = "HP Schwelle"
+L["Color health based on class."] = "Gesundheitsanzeige Farbe basierend auf Klasse"
+L["Health threshold"] = "Gesundheits Grenzwert"
 L["Only show deficit above % damage."] = "Zeige nur Defizit über % Schaden"
 L["Color deficit based on class."] = "Farben defizit basierend auf Klasse"
-L["Low HP threshold"] = "Wenig HP Schwelle"
-L["Set the HP % for the low HP warning."] = "Setzt die HP % für die wenig Gesundheit Warnung"
+L["Low HP threshold"] = "Wenig Gesundheit Grenzwert"
+L["Set the HP % for the low HP warning."] = "Setzt die Gesundheit % für die wenig Gesundheit Warnung"
 --}}}
 
 --{{{ GridStatusPvp
