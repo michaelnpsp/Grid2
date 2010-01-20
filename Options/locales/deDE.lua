bbcabf3bcd6238062e93bf0626262ae4f0dc15df
jerry <jerry@98ddf5e5-c516-4155-a43e-68834e374b74>
2010-01-20 12:52:53 +0000

    Remove isDefault flag, as it prevent the deDE locale to load

--- Options/locales/deDE.lua
+++ Options/locales/deDE.lua
@@ -1,4 +1,4 @@
-local L =  LibStub:GetLibrary("AceLocale-3.0"):NewLocale("Grid2Options", "deDE", true)
+local L =  LibStub:GetLibrary("AceLocale-3.0"):NewLocale("Grid2Options", "deDE")
 if not L then return end
 
 L["Debug"] = "Debug"
