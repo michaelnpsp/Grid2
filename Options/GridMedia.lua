63eea82b68e16218db86ea462133c3111a0f39c2
jerry <jerry@98ddf5e5-c516-4155-a43e-68834e374b74>
2010-01-22 09:03:52 +0000

    Fix SharedMedia handling. Fixes Ticket-80 adn Ticket-39

--- Options/GridMedia.lua
+++ Options/GridMedia.lua
@@ -3,32 +3,35 @@ local media = LibStub("LibSharedMedia-3.0", true)
 if not media then return end
 
 local registeredMedia = {}
+local dummy_info = {}
 
 media.RegisterCallback(Grid2Options, "LibSharedMedia_Registered", function (_, type, name)
 	local t = registeredMedia[type]
 	if not t then return end
 	local list = media:List(type)
 	for _, option in ipairs(t) do
-		if option:get() == name then
-			option:set(name)
-		end
+		dummy_info.option = option
 		option.values = list
+		if option.get(dummy_info) == name then
+			option.set(dummy_info, name)
+		end
 	end
 end)
 media.RegisterCallback(Grid2Options, "LibSharedMedia_SetGlobal", function (_, type)
 	local t = registeredMedia[type]
 	if not t then return end
 	for _, option in ipairs(t) do
-		option:set(option:get())
+		dummy_info.option = option
+		option.set(dummy_info, option.get(dummy_info))
 	end
 end)
 
 function Grid2Options:AddMediaOption(type, option)
+	option.values = media:List(type)
 	local t = registeredMedia[type]
 	if not t then
 		t = {}
 		registeredMedia[type] = t
 	end
 	t[#t + 1] = option
-	option.values = media:List(type)
 end
