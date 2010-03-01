18b0f025fca0895c5cba34aa8222cfe9b16456d9
StingerSoft <StingerSoft@98ddf5e5-c516-4155-a43e-68834e374b74>
2010-03-01 22:29:30 +0000

    ruRu update

--- trunk/RaidDebuffsOptions/locales/ruRU.lua
+++ trunk/RaidDebuffsOptions/locales/ruRU.lua
@@ -1,4 +1,4 @@
 ﻿local L =  LibStub:GetLibrary("AceLocale-3.0"):NewLocale("Grid2Options", "ruRU")
 if not L then return end
 
-L["raid-debuffs"] = "raid-debuffs"
+L["raid-debuffs"] = "Дебаффы-рейда"
