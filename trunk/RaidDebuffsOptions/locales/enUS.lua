15232abe90fa14d091d55a0697c61f7dd0a2e5bb
AZETHOTH <AZETHOTH@98ddf5e5-c516-4155-a43e-68834e374b74>
2010-03-10 22:59:50 +0000

    silence non-existence errors

--- trunk/RaidDebuffsOptions/locales/enUS.lua
+++ trunk/RaidDebuffsOptions/locales/enUS.lua
@@ -1,4 +1,4 @@
-local L =  LibStub:GetLibrary("AceLocale-3.0"):NewLocale("Grid2Options", "enUS", true)
+local L =  LibStub:GetLibrary("AceLocale-3.0"):NewLocale("Grid2Options", "enUS", true, true)
 if not L then return end
 
 L["raid-debuffs"] = true
