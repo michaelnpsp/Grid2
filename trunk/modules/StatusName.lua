deb2f6440b61c65b8da92ee9b03132c51b6f8be4
AZETHOTH <AZETHOTH@98ddf5e5-c516-4155-a43e-68834e374b74>
2010-04-01 23:34:12 +0000

    Add LuaTexts formatting hook

--- trunk/modules/StatusName.lua
+++ trunk/modules/StatusName.lua
@@ -2,38 +2,27 @@ local Name = Grid2.statusPrototype:new("name")
 
 function Name:OnEnable()
 	self:RegisterEvent("UNIT_NAME_UPDATE")
---	self:RegisterMessage("Grid_UnitJoined")
---	self:RegisterMessage("Grid_UnitChanged")
 end
 
 function Name:UNIT_NAME_UPDATE(_, unit)
 	return self:UpdateIndicators(unit)
 end
 
--- function Name:Grid_UnitJoined(_, unitid, guid)
-	-- return self:UpdateIndicators(unitid)
--- end
-
--- function Name:Grid_UnitChanged(_, unitid, guid)
-	-- return self:UpdateIndicators(unitid)
--- end
-
 function Name:OnDisable()
 	self:UnregisterEvent("UNIT_NAME_UPDATE")
---	self:UnregisterMessage("Grid_UnitJoined")
---	self:UnregisterMessage("Grid_UnitChanged")
 end
 
 function Name:IsActive(unit)
 	return true
 end
 
-function Name:GetText(unit)
+function Name:GetTextDefault(unit)
 	return UnitName(unit)
 end
 
 local function Create(baseKey, dbx)
 	Grid2:RegisterStatus(Name, {"text"}, baseKey, dbx)
+	Grid2:MakeTextHandler(Name)
 
 	return Name
 end
