a02c49f4f5558df1aa47bd264e1c93b0aca2f7b2
jerry <jerry@98ddf5e5-c516-4155-a43e-68834e374b74>
2011-01-07 09:55:47 +0000

    Add RaidDebuffs for Cataclysm, based on Ticket-199.

--- RaidDebuffs/Grid2StatusRaidDebuffs.lua
+++ RaidDebuffs/Grid2StatusRaidDebuffs.lua
@@ -327,6 +327,84 @@ local spellDB = {
 		27177,--Defile
 		68980,--Harvest Soul
 	},
+	[BZ["Blackwing Descent"]] = {
+		-- Magmaw
+		89773, -- Mangle
+		94679, -- Parasitic Infection
+
+		-- Omnitron Defense System
+		79889, -- Lightning Conductor
+		80161, -- Chemical Cloud
+		80011, -- Soaked in Poison
+		91535, -- Flamethrower
+		91829, -- Fixate
+		92035, -- Acquiring Target
+
+		--Maloriak
+		92991, -- Rend
+		-- Acid (?)
+		92910, -- Debilitating Slime
+		77786, -- Consuming Flames
+		91829, -- Fixate
+		77760, -- Biting Chill
+		77699, -- Flash Freeze
+
+		-- Atramedes
+		78092, -- Tracking
+		77840, -- Searing
+		78353, -- Roaring Flame
+		78897, -- Noisy
+
+		-- Chimaeron
+		89084, -- Low Health
+		82934, -- Mortality
+		88916, -- Caustic Slime
+		82881, -- Break
+
+		-- Nefarian
+		94075, -- Magma
+		77827, -- Tail Lash
+	},
+	[BZ["The Bastion of Twilight"]] = {
+		-- Halfus Wyrmbreaker
+		83908, -- Malevolent Strike
+		83603, -- Stone Touch
+
+		-- Valiona & Theralion
+		86788, -- Blackout
+		95639, -- Engulfing Magic
+		86360, -- Twilight Shift
+
+		-- Ascendant Council
+		82762, -- Waterlogged
+		83099, -- Lightning Rod
+		-- Elemental Statis (?)
+		82660, -- Burning Blood
+		82665, -- Heart of Ice
+
+		-- Cho'gall
+		93187, -- Corrupted Blood
+		82523, -- Gall's Blast
+		82518, -- Cho's Blast
+		93134, -- Debilitating Beam
+	},
+	[BZ["Throne of the Four Winds"]] = {
+		-- Conclave of Wind
+		84645, -- Wind Chill
+		86107, -- Ice Patch
+		86082, -- Permafrost
+		84643, -- Hurricane
+		86281, -- Toxic Spores
+		85573, -- Deafening Winds
+		85576, -- Withering Winds
+
+		-- Al'Akir
+		88290, -- Acid Rain
+		87873, -- Static Shock
+		88427, -- Electrocute
+		89668, -- Lightning Rod
+	},
+
 }
 
 local GSRD = Grid2:NewModule("StatusRaidDebuffs")
@@ -478,7 +556,7 @@ function Grid2:LoadOptions(dblData, ...)
 	local upgrade = prev_LoadOptions(self, dblData, ...)
 
 	upgrade = DBL:LoadOptions("Grid2StatusRaidDebuffsOptions", dblData, nil, "account", 1) or upgrade
-	
+
 	return upgrade
 end
 
@@ -491,7 +569,7 @@ function Grid2:UpgradeDefaults(dblData, ...)
 	if (Grid2StatusRaidDebuffsOptions) then
 		flatten = DBL:UpgradeDefaults("Grid2StatusRaidDebuffsOptions", dblData, Grid2StatusRaidDebuffsOptions.UpgradeDefaults, "account", 1) or flatten
 	end
-	
+
 	return flatten
 end
 
