f84c25efeaca152a1d96b149c7f88cf50e7aee11
michaelspain <michaelspain@98ddf5e5-c516-4155-a43e-68834e374b74>
2011-05-07 22:45:58 +0000

    Adding missing raid debuffs, thanks to kaleide

--- RaidDebuffsOptions/RaidDebuffsCataclysm.lua
+++ RaidDebuffsOptions/RaidDebuffsCataclysm.lua
@@ -5,6 +5,8 @@ RDDB["Cataclysm"] = {
 		["Magmaw"]= {
 		89773, -- Mangle
 		94679, -- Parasitic Infection
+		88287, -- Massive Crash
+        78199, -- Sweltering Armor		
 		},
 		["Omnitron Defense System"]= {
 		79889, -- Lightning Conductor
@@ -13,6 +15,9 @@ RDDB["Cataclysm"] = {
 		91535, -- Flamethrower
 		91829, -- Fixate
 		92035, -- Acquiring Target
+        92053, -- Shadow Conductor
+        92048, -- Shadow Infusion
+        92023, -- Encasing Shadows		
 		},
 		["Maloriak"]= {
 		92991, -- Rend
@@ -22,6 +27,8 @@ RDDB["Cataclysm"] = {
 		91829, -- Fixate
 		77760, -- Biting Chill
 		77699, -- Flash Freeze
+        92987, -- Dark Sludge
+        92982, -- Engulfing Darkness		
 		},
 		["Atremedes"]= {
 		78092, -- Tracking
@@ -34,14 +41,18 @@ RDDB["Cataclysm"] = {
 		82934, -- Mortality
 		88916, -- Caustic Slime
 		82881, -- Break
+        91307, -- Mocking Shadows		
 		},
 		["Nefarian"]={
 		94075, -- Magma
 		77827, -- Tail Lash
+        79339, -- Explosive Cinders
+        79318, -- Dominion		
 		},
 	},
 	["The Bastion of Twilight"] = {
 		["Halfus Wyrmbreaker"]= {
+		83710, -- Furious Roar		
 		83908, -- Malevolent Strike
 		83603, -- Stone Touch
 		},
@@ -49,6 +60,8 @@ RDDB["Cataclysm"] = {
 		86788, -- Blackout
 		95639, -- Engulfing Magic
 		86360, -- Twilight Shift
+        86014, -- Twilight Meteorite
+        92886, -- Twilight Zone		
 		},
 		["Ascendant Council"]= {
 		82762, -- Waterlogged
@@ -56,8 +69,18 @@ RDDB["Cataclysm"] = {
 		82285, -- Elemental Stasis
 		82660, -- Burning Blood
 		82665, -- Heart of Ice
+        82772, -- Frozen
+        84948, -- Gravity Crush
+        83500, -- Swirling Winds
+        83581, -- Grounded
+        92307, -- Frost Beacon
+        92467, -- Static Overload
+        92538, -- Gravity Core		
 		},
 		["Cho'gall"]= {
+        81836, -- Corruption: Accelerated
+        82125, -- Corruption: Malformation
+        82170, -- Corruption: Absolute		
 		93187, -- Corrupted Blood
 		82523, -- Gall's Blast
 		82518, -- Cho's Blast
@@ -73,12 +96,20 @@ RDDB["Cataclysm"] = {
 		86281, -- Toxic Spores
 		85573, -- Deafening Winds
 		85576, -- Withering Winds
+		93057, -- Slicing Gale		
 		},
 		["Al'Akir"]= {
 		88290, -- Acid Rain
 		87873, -- Static Shock
 		88427, -- Electrocute
 		89668, -- Lightning Rod
+        87856, -- Squall Line		
 		},
 	},
+    ["Baradin Hold"] = {
+        ["Argaloth"]= {
+        88942, -- Meteor Slash
+        88954, -- Consuming Darkness
+        },
+    },	
 }
