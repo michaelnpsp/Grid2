1974cabc8eda1cf55e6c6e655f4bb7a589ca6204
michaelspain <michaelspain@98ddf5e5-c516-4155-a43e-68834e374b74>
2011-07-06 15:31:53 +0000

    Making color statuses compatible with percent indicators. Increasing frame levels a indicator can be assigned to.

--- trunk/modules/StatusColor.lua
+++ trunk/modules/StatusColor.lua
@@ -14,13 +14,18 @@ local function Color_GetColor(self)
 	return c.r, c.g, c.b, c.a
 end
 
+local function Color_GetPercent(self)
+	return self.dbx.color1.a
+end
+
 local function CreateColor(baseKey, dbx)
 	local status = Grid2.statusPrototype:new(baseKey, false)
 	status.OnEnable = Color_Dummy
 	status.OnDisable= Color_Dummy
 	status.IsActive = Color_IsActive
 	status.GetColor = Color_GetColor
-	Grid2:RegisterStatus(status, {"color"}, baseKey, dbx)
+	status.GetPercent= Color_GetPercent
+	Grid2:RegisterStatus(status, {"color","percent"}, baseKey, dbx)
 	return status
 end
 
