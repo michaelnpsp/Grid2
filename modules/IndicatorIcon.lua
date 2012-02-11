ce7c0a37326955c9b0c9e42e6993dfa2cf9d5ffa
michaelspain <michaelspain@98ddf5e5-c516-4155-a43e-68834e374b74>
2012-02-11 06:55:28 +0000

    Fixing a bug in icon-indicators: Stacks text gets hidden when a debuff has no expiration or duration time and cooldown animation is enabled.

--- modules/IndicatorIcon.lua
+++ modules/IndicatorIcon.lua
@@ -26,10 +26,10 @@ local function Icon_Create(self, parent)
 			Cooldown = f.Cooldown or CreateFrame("Cooldown", nil, f, "CooldownFrameTemplate")
 			Cooldown.noCooldownCount = true 
 		else
-			local name= self.name:gsub("%-","")
+			local name = self.name:gsub("%-","")
 			local i,j = parent:GetName():match("Grid2LayoutHeader(%d+)UnitButton(%d+)")
 			Cooldown = f.Cooldown or CreateFrame("Cooldown", fmt("Grid2%s%02d%02d",name,i,j) , f, "CooldownFrameTemplate")
-			Cooldown.noCooldownCount= nil
+			Cooldown.noCooldownCount = nil
 		end
 		Cooldown:SetReverse(self.dbx.reverseCooldown)
 		Cooldown:Hide()
@@ -37,11 +37,22 @@ local function Icon_Create(self, parent)
 	end
 	
 	if not self.disableStack then
-		local CooldownText = f.CooldownText or f:CreateFontString(nil, "OVERLAY")
+		local TextFrame
+		if self.disableCooldown then
+			if f.TextFrame then f.TextFrame:Hide() end
+			TextFrame = f
+		else
+			TextFrame = f.TextFrame or CreateFrame("Frame", nil, f)
+			TextFrame:SetAllPoints()
+			TextFrame:Show()
+			f.TextFrame = TextFrame	
+		end
+		local CooldownText = f.CooldownText or f:CreateFontString(nil, "OVERLAY")	
+		CooldownText:SetParent(TextFrame)
 		CooldownText:SetFontObject(GameFontHighlightSmall)
 		local font = Grid2:MediaFetch("font", self.dbx.font) or CooldownText:GetFont()
 		CooldownText:SetFont(font, self.dbx.fontSize, "OUTLINE" )
-		local c= self.dbx.stackColor
+		local c = self.dbx.stackColor
 		if c then CooldownText:SetTextColor(c.r, c.g, c.b, c.a) end	
 		CooldownText:Hide()
 		f.CooldownText = CooldownText
@@ -99,17 +110,19 @@ end
 
 local function Icon_Layout(self, parent)
 	local f = parent[self.name]
+
+	local level = parent:GetFrameLevel() + self.frameLevel
 	f:ClearAllPoints()
-	f:SetFrameLevel(parent:GetFrameLevel() + self.frameLevel)
 	f:SetPoint(self.anchor, parent.container, self.anchorRel, self.offsetx, self.offsety)
+	f:SetFrameLevel(level)
 
 	local Icon = f.Icon
 	local r,g,b,a = f:GetBackdropBorderColor()
 	local backdrop = f:GetBackdrop()
 	local borderSize = self.borderSize
 	if borderSize then
-		Icon:SetPoint("TOPLEFT", f ,"TOPLEFT", borderSize, -1 * borderSize)
-		Icon:SetPoint("BOTTOMRIGHT", f ,"BOTTOMRIGHT", -1 * borderSize, borderSize)
+		Icon:SetPoint("TOPLEFT", f ,"TOPLEFT", borderSize, -borderSize)
+		Icon:SetPoint("BOTTOMRIGHT", f ,"BOTTOMRIGHT", -borderSize, borderSize)
 		backdrop.edgeSize = borderSize
 	else
 		Icon:SetAllPoints(f)
@@ -127,18 +140,19 @@ local function Icon_Layout(self, parent)
 	f:SetSize(size,size)
 	
 	if not self.disableStack then
+		if f.TextFrame then	f.TextFrame:SetFrameLevel(level+1) end
 		local CooldownText = f.CooldownText
 		local justifyH = self.dbx.fontJustifyH or "CENTER"
 		local justifyV = self.dbx.fontJustifyV or "MIDDLE"
-		CooldownText:SetParent( self.disableCooldown and f or f.Cooldown )
 		CooldownText:SetJustifyH( justifyH )
 		CooldownText:SetJustifyV( justifyV  )
 		CooldownText:ClearAllPoints()
 		CooldownText:SetPoint("TOP")
 		CooldownText:SetPoint("BOTTOM")
 		CooldownText:SetPoint("LEFT" , justifyH=="LEFT"  and 0 or -size, 0)
-		CooldownText:SetPoint("RIGHT", justifyH=="RIGHT" and 0 or  size, 0)
+		CooldownText:SetPoint("RIGHT", justifyH=="RIGHT" and 2 or  size+2, 0)
 	end
+	
 end
 
 local function Icon_Disable(self, parent)
