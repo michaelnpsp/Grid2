8766a88e5451ab6b648ef13e92a01c789b209db9
gragagrogog <gragagrogog@98ddf5e5-c516-4155-a43e-68834e374b74>
2013-06-25 04:07:55 +0000

    RaidDebuffs:  -added EncounterJournal link to each boss/instance with an EJ section  -boss names are now localized(pulled from EJ)  -few more changes to make it more user friendly StatusDirection:  -more filtering options added

--- RaidDebuffsOptions/RaidDebuffsMoP.lua
+++ RaidDebuffsOptions/RaidDebuffsMoP.lua
@@ -1,39 +1,39 @@
 local RDDB = Grid2Options:GetRaidDebuffsTable()
 
 RDDB["Mists of Pandaria"] = {
-    [897] = {
+    [897] = { --Heart of Fear
         ["Trash"] = { 
         },
-        ["Imperial Vizier Zor'lok"] = {
+        ["[745-1]Imperial Vizier Zor'lok"] = {
         122760, --Exhale
         123812, --Pheromones of Zeal
         122740, --Convert
         122706, --Noise Cancelling        
         },
-        ["Blade Lord Ta'yak"] = {
+        ["[744-2]Blade Lord Ta'yak"] = {
         122949, --Unseen Strike
         123474, --Overwhelming Assault
         124783, --Storm Unleashed
         123600, --Storm Unleashed?        
         },
-        ["Garalon"] = {
+        ["[713-3]Garalon"] = {
         122835, --Pheromones
         123081, --Pungency
         123120, --Pheromone Trail        
         },
-        ["Wind Lord Mel'jarak"] = {
+        ["[741-4]Wind Lord Mel'jarak"] = {
         29212,--Cripple (NH)
         121881, --Amber Prison
         122055, --Residue
         122064, --Corrosive Resin        
         },
-        ["Amber-Shaper Un'sok"] = {
+        ["[737-5]Amber-Shaper Un'sok"] = {
         121949, --Parasitic Growth
         122784, --Reshape Life
         122064, --Corrosive Resin
         122504, --Burning Amber        
         },
-        ["Grand Empress Shek'zeer"] = {
+        ["[743-6]Grand Empress Shek'zeer"] = {
         125390, --Fixate
         123707, --Eyes of the Empress
         123788, --Cry of Terror
@@ -49,12 +49,12 @@ RDDB["Mists of Pandaria"] = {
         123846, --Heart of Fear: Lure        
         },
     },
-    [896] = {
+    [896] = { --Mogu'shan Vaults
         ["Trash"] = {
         118562, --Petrified
         116596, --Smoke Bomb
         },
-        ["The Stone Guard"] = {
+        ["[679-1]The Stone Guard"] = {
         130395, --Jasper Chains: Stacks
         130404, --Jasper Chains
         130774, --Amethyst Pool
@@ -65,7 +65,7 @@ RDDB["Mists of Pandaria"] = {
         125206, --Rend Flesh: Tank only
         116008, --Jade Petrification
         },
-        ["Feng The Accursed"] = {
+        ["[689-2]Feng The Accursed"] = {
         131788, --Lightning Lash: Tank Only: Stacks
         116040, --Epicenter
         116942, --Flaming Spear: Tank Only
@@ -77,24 +77,24 @@ RDDB["Mists of Pandaria"] = {
         116374, --Lightning Charge: Stun effect
         131792, --Shadowburn: Tank only: Stacks: HEROIC ONLY
         },
-        ["Gara'jal the Spiritbinder"] = {
+        ["[682-3]Gara'jal the Spiritbinder"] = {
         122151, --Voodoo doll: Super Super Important! Like Holy jesus important!
         117723, --Frail Soul: HEROIC ONLY
         116260, --Crossed Over
         },
-        ["The Spirit Kings"] = {
+        ["[687-4]The Spirit Kings"] = {
         118303, --Undying Shadow: Fixate
         118048, --Pillaged
         118135, --Pinned Down
         118047, --Pillage: Target
         118163, --Robbed Blind
         },
-        ["Elegon"] = {
+        ["[726-5]Elegon"] = {
         117878, --Overcharged
         117949, --Closed circuit (dispellable)
         117945, --Arcing Energy
         },
-        ["Will of the Emperor"] = {
+        ["[677-6]Will of the Emperor"] = {
         116525, --Focused Assault
         116778, --Focused Defense
         117485, --Impeding Thrust
@@ -102,25 +102,25 @@ RDDB["Mists of Pandaria"] = {
         116829, --Focused Energy
         },
     },
-    [809] = {
-        ["Sha of Anger"] = {
+    [809] = { --Kun-Lai Summit
+        ["[691-]Sha of Anger"] = {
         119626, --Aggressive Behavior [NOTE: this is the MC]
         119488, --Unleashed Wrath [NOTE: Must heal these people. Lots of shadow dmg]
         119610, --Bitter Thoughts (Silence)
         119601, --Bitter Thoughts (Silence)
         },        
     },
-    [886] = {
+    [886] = { --Terrace of Endless Spring
         ["Trash"] = {
         },
-        ["Protector Kaolan"] = {
+        ["[683-1]Protector Kaolan"] = {
         117519, --Touch of Sha
         111850, --Lightning Prison: Targeted
         117436, --Lightning Prison: Stunned
         118191, --Corrupted Essence
         117986, --Defiled Ground: Stacks
         },
-        ["Tsulong"] = {
+        ["[742-2]Tsulong"] = {
         122768, --Dread Shadows
         122777, --Nightmares (dispellable)
         122752, --Shadow Breath
@@ -130,11 +130,11 @@ RDDB["Mists of Pandaria"] = {
         123036, --Fright (dispellable)
         122858, --Bathed in Light
         },
-        ["Lei Shi"] = {
+        ["[729-3]Lei Shi"] = {
         123121, --Spray
         123705, --Scary Fog
         },
-        ["Sha of Fear"] = {
+        ["[709-4]Sha of Fear"] = {
         119414, --Breath of Fear
         129147, --Onimous Cackle
         119983, --Dread Spray
@@ -147,16 +147,16 @@ RDDB["Mists of Pandaria"] = {
         119775, --Reaching Attack
         },        
     },
-    [930] = {
+    [930] = { --Throne of Thunder
     	["Trash"] = {
     	},
-    	["Jin'rokh the Breaker"] = {
+    	["[827-1]Jin'rokh the Breaker"] = {
     	138349, --Static Wound
     	137399, --Focused Lightning
     	138733, --Ionization
     	138002, --Fluidity
     	},
-    	["Horridon"] = {
+    	["[819-2]Horridon"] = {
     	136767, --Triple Puncture
     	136708, --Stone Gaze
     	136719, --Blazing Sunlight
@@ -166,7 +166,7 @@ RDDB["Mists of Pandaria"] = {
     	140946, --Dire Fixation
     	136710, --Deadly Plague
     	},
-    	["Council of Elders"] = {
+    	["[816-3]Council of Elders"] = {
     	137650, --Shadowed Soul
     	137085, --Chilled to the Bone
     	136922, --Frostbite
@@ -176,10 +176,10 @@ RDDB["Mists of Pandaria"] = {
     	137359, --Marked Soul
     	137891, --Twisted Fate
     	},
-    	["Tortos"] = {
+    	["[825-4]Tortos"] = {
     	137552, --Crystal Shell
     	},
-    	["Megaera"] = {
+    	["[821-5]Megaera"] = {
     	139822, --Cinders
     	137731, --Ignite Flesh
     	139866, --Torrent of Ice
@@ -189,14 +189,14 @@ RDDB["Mists of Pandaria"] = {
     	140179, --Suppression
     	139994, --Diffusion
     	},
-    	["Ji-Kun"] = {
+    	["[828-6]Ji-Kun"] = {
     	140092, --Infected Talons
     	134256, --Slimed
     	138319, --Feed Pool
     	134366, --Talon Rake
     	140014, --Daedelian Wings
     	},
-    	["Durumu the Forgotten"] = {
+    	["[818-7]Durumu the Forgotten"] = {
     	133767, --Serious Wound
     	133768, --Arterial Cut
     	134755, --Eye Sore
@@ -206,31 +206,31 @@ RDDB["Mists of Pandaria"] = {
     	133598, --Dark Plague
     	134007, --Devour
     	},
-    	["Primordius"] = {
+    	["[820-8]Primordius"] = {
     	136050, --Malformed Blood
     	140546, --Fully Mutated
     	137000, --Black Blood
     	136228, --Volatile Pathogen
     	},
-    	["Dark Animus"] = {
+    	["[824-9]Dark Animus"] = {
     	138609, --Matter Swap
     	138569, --Explosive Slam
     	138659, --Touch of the Animus
     	136954, --Anima Ring
     	},
-    	["Iron Qon"] = {
+    	["[817-10]Iron Qon"] = {
     	134691, --Impale
     	134647, --Scorched
     	136193, --Arcing Lightning
     	135145, --Freeze
     	},
-    	["Twin Consorts"] = {
+    	["[829-11]Twin Consorts"] = {
     	137341, --Beast of Nightmares
     	137360, --Corrupted Healing
     	137408, --Fan of Flames
     	137440, --Icy Shadows
     	},
-    	["Lei Shen"] = {
+    	["[832-12]Lei Shen"] = {
     	134916, --Decapitate
     	135150, --Crashing Thunder
     	139011, --Helm of Command
@@ -239,7 +239,44 @@ RDDB["Mists of Pandaria"] = {
     	136295, --Overcharged
     	135703, --Static Shock
     	},
-    	["Ra-den"] = {
+    	["[831-13]Ra-den"] = {
     	}
-    }
+    },
+	[929] = { --Isle of Giants
+		["[826-]Oondasta"] = {
+		137504, --Crush
+		},
+	},
+	[953] = { --Siege of Orgrimmar
+		["[852-1]Immerseus"] = {
+		},
+		["[849-2]The Fallen Protectors"] = {
+		},
+		["[866-3]Norushen"] = {
+		},
+		["[867-4]Sha of Pride"] = {
+		},
+		["[868-5]Galakras"] = {
+		},
+		["[864-6]Iron Juggernaut"] = {
+		},
+		["[856-7]Kor'kron Dark Shaman"] = {
+		},
+		["[850-8]General Nazgrim"] = {
+		},
+		["[846-9]Malkorok"] = {
+		},
+		["[870-10]Spoils of Pandaria"] = {
+		},
+		["[851-11]Thok the Bloodthirsty"] = {
+		},
+		["[865-12]Siegecrafter Blackfuse"] = {
+		},
+		["[853-13]Paragons of the Klaxxi"] = {
+		},
+		["[869-14]Garrosh Hellscream"] = {
+		},
+		["Trash"] = {
+		},
+	},
 }
