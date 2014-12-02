e85a6f5d128930a4ff5ab4da800bd6d829d137cf
gragagrogog <gragagrogog@98ddf5e5-c516-4155-a43e-68834e374b74>
2014-12-02 07:33:39 +0000

    Added WoD RaidDebuffs structure (only categories, no debuffs added yet) for Highmaul and Blackrock Foundry.

---
+++ RaidDebuffsOptions/RaidDebuffsWoD.lua
@@ -0,0 +1,63 @@
+local RDDB = Grid2Options:GetRaidDebuffsTable()
+
+RDDB["Warlords of Draenor"] = {
+	[994] = { --Highmaul
+		["[1128-1]Kargath Bladefist"] = {
+
+		},
+		["[971-2]The Butcher"] = {
+
+		},
+		["[1195-3]Tectus"] = {
+
+		},
+		["[1196-4]Brackenspore"] = {
+
+		},
+		["[1148-5]Twin Ogron"] = {
+
+		},
+		["[1153-6]Ko'ragh"] = {
+
+		},
+		["[1197-7]Imperator Mar'gok"] = {
+
+		},
+		["Trash"] = { 
+		},
+	},
+	[988] = { --Blackrock Foundry
+		["[1161-1]Gruul"] = {
+
+		},
+		["[1202-2]Oregorger"] = {
+
+		},
+		["[1122-3]Beastlord Darmac"] = {
+
+		},
+		["[1123-4]Flamebender Ka'graz"] = {
+
+		},
+		["[1155-5]Hans'gar and Franzok"] = {
+
+		},
+		["[1147-6]Operator Thogar"] = {
+
+		},
+		["[1154-7]The Blast Furnace"] = {
+
+		},
+		["[1162-8]Kromog"] = {
+
+		},
+		["[1203-9]The Iron Maidens"] = {
+
+		},
+		["[959-10]Blackhand"] = {
+
+		},
+		["Trash"] = { 
+		},
+	}
+}
