2c83c28e77bb54e47270dca8fea5146e1190c4c7
michaelspain <michaelspain@98ddf5e5-c516-4155-a43e-68834e374b74>
2019-07-21 20:50:57 +0000

    -Alpha indicator: Now the default alpha can be configured (alpha when the indicator is not active), previously it was always full opaque(1). -Added an "Hide in Pet Battle" option in General>Appearance -Fixed a bug in "Show Frame" option, It was not working when "Display all groups" (in layout editor section) option was enabled.

--- trunk/Options/modules/indicators/IndicatorAlpha.lua
+++ trunk/Options/modules/indicators/IndicatorAlpha.lua
@@ -8,25 +8,42 @@ Grid2Options:RegisterIndicatorOptions("alpha",  false, function(self, indicator)
 end)
 
 function Grid2Options:MakeIndicatorAlphaOptions(indicator,options)
-	options.ignore = {
-		type = "toggle",
-		name = "|cffffd200".. L["Use Global Alpha"] .."|r",
-		desc = L["Discard opacity value provided by the statuses and instead use a global user defined opacity."],
+	options.header1 = { type = "header", order = 5, name = "Default Alpha" }
+	options.defaultAlpha = {
+		type = "range",
 		order = 10,
 		width = "normal",
+		name = L["Default Alpha Value"],
+		desc = L["Alpha/opacity when the indicator is not activated.\n0 = full transparent\n1 = full opaque"],
+		min = 0,
+		max = 1,
+		step = 0.01,
+		get = function () return indicator.dbx.defaultAlpha or 1 end,
+		set = function (_, v) 
+			indicator.dbx.defaultAlpha = v<.999 and v or nil	
+			self:RefreshIndicator(indicator, "Update")
+		end,
+	}
+	options.header2 = { type = "header", order = 15, name = "Active Alpha" }
+	options.alphaMode = {
+		type = "toggle",
+		name = L["Use Status Alpha"],
+		desc = L["Check this option to use the alpha value provided by the active status."],
+		order = 40,
+		width = "normal",
 		tristate = false,
-		get = function () return indicator.dbx.alpha~=nil end,
+		get = function () return indicator.dbx.alpha==nil end,
 		set = function (_, v)
-			indicator.dbx.alpha = v and 0.25 or nil
+			indicator.dbx.alpha = (not v) and 0.4 or nil
 			self:RefreshIndicator(indicator, "Update")
 		end,
 	}
 	options.alpha = {
 		type = "range",
-		order = 20,
+		order = 30,
 		width = "normal",
-		name = L["Alpha Value"],
-		desc = L["Alpha/Opacity value to apply to the frame when the indicator is enabled.\n0 = full transparent\n1 = full opaque"],
+		name = L["Active Alpha Value"],
+		desc = L["Alpha/Opacity value to apply to the frame when the indicator is activated.\n0 = full transparent\n1 = full opaque"],
 		min = 0,
 		max = 1,
 		step = 0.01,
@@ -35,6 +52,6 @@ function Grid2Options:MakeIndicatorAlphaOptions(indicator,options)
 			indicator.dbx.alpha = v	
 			self:RefreshIndicator(indicator, "Update")
 		end,
-		hidden = function() return not indicator.dbx.alpha end,
+		disabled = function() return indicator.dbx.alpha==nil end,
 	}
 end
--- trunk/modules/IndicatorAlpha.lua
+++ trunk/modules/IndicatorAlpha.lua
@@ -1,18 +1,19 @@
 local Alpha = Grid2.indicatorPrototype:new("alpha")
 
-local defaultAlpha
+local defaultAlpha = 1
+local enabledAlpha = 0.5
 
 Alpha.Create = Grid2.Dummy
 Alpha.Layout = Grid2.Dummy
 
 -- standard update, opacity value is provided by the active status 
 local function Alpha_OnUpdate1(self, parent, unit, status)
-	parent:SetAlpha(status and status:GetPercent(unit) or 1)
+	parent:SetAlpha(status and status:GetPercent(unit) or defaultAlpha)
 end
 
 -- optional update, alpha provided by the statuses is ignored and instead the opacity defined in the indicator setup is used
 local function Alpha_OnUpdate2(self, parent, unit, status)
-	parent:SetAlpha(status and defaultAlpha or 1)
+	parent:SetAlpha(status and enabledAlpha or defaultAlpha)
 end
 
 function Alpha:Disable(parent)
@@ -20,8 +21,9 @@ function Alpha:Disable(parent)
 end
 
 function Alpha:UpdateDB()
-	defaultAlpha = self.dbx.alpha
-	self.OnUpdate = defaultAlpha and Alpha_OnUpdate2 or Alpha_OnUpdate1
+	defaultAlpha = self.dbx.defaultAlpha or 1
+	enabledAlpha = self.dbx.alpha
+	self.OnUpdate = enabledAlpha and Alpha_OnUpdate2 or Alpha_OnUpdate1
 end
 
 local function Create(indicatorKey, dbx)
