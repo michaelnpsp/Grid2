18a41418be40ec33dec1334958ad57925abfd130
michaelsp <michael.npsp@gmail.com>
2022-09-19 05:49:40 +0200

    Added Shine effect in options for Glowing Border indicator.

--- Options/modules/indicators/IndicatorBorderGlow.lua
+++ Options/modules/indicators/IndicatorBorderGlow.lua
@@ -3,7 +3,6 @@ local L = Grid2Options.L
 local DEFAULT_FREQS = { 0.25, 0.12, 0.12 }
 local COLOR_VALUES = { [1] = L["Status Color"], [2] = L["Custom Color"] }
 local EFFECT_VALUES = { [1] = L['Pixel'], [2] = L['Shine'], [3] = L["Blizzard"] }
-if Grid2.isClassic then EFFECT_VALUES[2] = nil end
 
 local function CheckBlizzardGlowEffectNotUsed(indExcluded)
 	for _,indicator in Grid2:IterateIndicators() do
