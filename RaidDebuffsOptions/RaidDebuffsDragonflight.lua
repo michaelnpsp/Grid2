dbccc4ad22ee3020fa3e578ed43ce8567148d131
michaelsp <michael.npsp@gmail.com>
2022-11-11 01:40:33 +0100

    RaidDebuffs: Added Dragonflight module with dungeons and new raid but no debuffs yet.

---
+++ RaidDebuffsOptions/RaidDebuffsDragonflight.lua
@@ -0,0 +1,168 @@
+local RDDB = Grid2Options:GetRaidDebuffsTable()
+RDDB["Dragonflight"] = {
+	-- 5 man instances
+	[1201] = {
+		{ id = 1201, name = "Algeth'ar Academy" },
+		["Vexamus"] = {
+		order = 1, ejid = 2509,
+		},
+		["Overgrown Ancient"] = {
+		order = 2, ejid = 2512,
+		},
+		["Crawth"] = {
+		order = 3, ejid = 2495,
+		},
+		["Echo of Doragosa"] = {
+		order = 4, ejid = 2514,
+		},
+	},
+	[1196] = {
+		{ id = 1196, name = "Brackenhide Hollow" },
+		["Hackclaw's War-Band"] = {
+		order = 1, ejid = 2471,
+		},
+		["Treemouth"] = {
+		order = 2, ejid = 2473,
+		},
+		["Gutshot"] = {
+		order = 3, ejid = 2472,
+		},
+		["Decatriarch Wratheye"] = {
+		order = 4, ejid = 2474,
+		},
+	},
+	[1204] = {
+		{ id = 1204, name = "Halls of Infusion" },
+		["Watcher Irideus"] = {
+		order = 1, ejid = 2504,
+		},
+		["Gulping Goliath"] = {
+		order = 2, ejid = 2507,
+		},
+		["Khajin the Unyielding"] = {
+		order = 3, ejid = 2510,
+		},
+		["Primal Tsunami"] = {
+		order = 4, ejid = 2511,
+		},
+	},
+	[1199] = {
+		{ id = 1199, name = "Neltharus" },
+		["Chargath, Bane of Scales"] = {
+		order = 1, ejid = 2490,
+		},
+		["Forgemaster Gorek"] = {
+		order = 2, ejid = 2489,
+		},
+		["Magmatusk"] = {
+		order = 3, ejid = 2494,
+		},
+		["Warlord Sargha"] = {
+		order = 4, ejid = 2501,
+		},
+	},
+	[1202] = {
+		{ id = 1202, name = "Ruby Life Pools" },
+		["Melidrussa Chillworn"] = {
+		order = 1, ejid = 2488,
+		},
+		["Kokia Blazehoof"] = {
+		order = 2, ejid = 2485,
+		},
+		["Kyrakka and Erkhart Stormvein"] = {
+		order = 3, ejid = 2503,
+		},
+	},
+	[1203] = {
+		{ id = 1203, name = "The Azure Vault" },
+		["Leymor"] = {
+		order = 1, ejid = 2492,
+		},
+		["Azureblade"] = {
+		order = 2, ejid = 2505,
+		},
+		["Telash Greywing"] = {
+		order = 3, ejid = 2483,
+		},
+		["Umbrelskul"] = {
+		order = 4, ejid = 2508,
+		},
+	},
+	[1198] = {
+		{ id = 1198, name = "The Nokhud Offensive" },
+		["Granyth"] = {
+		order = 1, ejid = 2498,
+		},
+		["The Raging Tempest"] = {
+		order = 2, ejid = 2497,
+		},
+		["Teera and Maruuk"] = {
+		order = 3, ejid = 2478,
+		},
+		["Balakar Khan"] = {
+		order = 4, ejid = 2477,
+		},
+	},
+	[1197] = {
+		{ id = 1197, name = "Uldaman: Legacy of Tyr" },
+		["The Lost Dwarves"] = {
+		order = 1, ejid = 2475,
+		},
+		["Bromach"] = {
+		order = 2, ejid = 2487,
+		},
+		["Sentinel Talondras"] = {
+		order = 3, ejid = 2484,
+		},
+		["Emberon"] = {
+		order = 4, ejid = 2476,
+		},
+		["Chrono-Lord Deios"] = {
+		order = 5, ejid = 2479,
+		},
+	},
+	-- World Bosses
+	[102444] = {
+		{ id = 1205, name = "Dragon Isles", raid = true },
+		["Strunraan, The Sky's Misery"] = {
+		order = 1, ejid = 2515,
+		},
+		["Basrikron, The Shale Wing"] = {
+		order = 2, ejid = 2506,
+		},
+		["Bazual, The Dreaded Flame"] = {
+		order = 3, ejid = 2517,
+		},
+		["Liskanoth, The Futurebane"] = {
+		order = 4, ejid = 2518,
+		},
+	},
+	-- Raid instances
+	[1200] = {
+		{ id = 1200, name = "Vault of the Incarnates", raid = true },
+		["Eranog"] = {
+		order = 1, ejid = 2480,
+		},
+		["Terros"] = {
+		order = 2, ejid = 2500,
+		},
+		["The Primal Council"] = {
+		order = 3, ejid = 2486,
+		},
+		["Sennarth, the Cold Breath"] = {
+		order = 4, ejid = 2482,
+		},
+		["Dathea, Ascended"] = {
+		order = 5, ejid = 2502,
+		},
+		["Kurog Grimtotem"] = {
+		order = 6, ejid = 2491,
+		},
+		["Broodkeeper Diurna"] = {
+		order = 7, ejid = 2493,
+		},
+		["Raszageth the Storm-Eater"] = {
+		order = 8, ejid = 2499,
+		},
+	},
+}
