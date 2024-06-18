df3ed246718abca6dee44fa6da361e6565a87cb4
michaelsp <michael.npsp@gmail.com>
2024-06-18 02:56:48 +0200

    Fixing minimimap icon popup menu for "The War Within" Expansion.

--- LDB/Grid2LDB.lua
+++ LDB/Grid2LDB.lua
@@ -91,6 +91,17 @@ do
 			table.insert( menuTable, 1, { text = L["Grid2 Movement"],  notCheckable= true, isTitle = true } )
 		end
 	end
+	local EasyMenu_Initialize = EasyMenu_Initialize or function(frame, level, menuList)
+		for index = 1, #menuList do
+			local value = menuList[index]
+			if value.text then value.index = index; UIDropDownMenu_AddButton(value, level) end
+		end
+	end
+	local EasyMenu = EasyMenu or function(menuList, menuFrame, anchor, x, y, displayMode, autoHideDelay)
+		if displayMode=='MENU' then menuFrame.displayMode = displayMode end
+		UIDropDownMenu_Initialize(menuFrame, EasyMenu_Initialize, displayMode, nil, menuList)
+		ToggleDropDownMenu(1, nil, menuFrame, anchor, x, y, menuList, nil, autoHideDelay)
+	end
 	MenuLayoutsShow= function()
 		menuFrame= menuFrame or CreateFrame("Frame", "Grid2LDBLayoutsMenu", UIParent, "UIDropDownMenuTemplate")
 		CreateMenuTable()
