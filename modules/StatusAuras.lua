e9076d75caa3a7868903ced30de0d5d79ddcfc13
RedAces <redaces@gmx.de>
2025-01-13 19:29:47 +0100

    add LibDispel too for non-retail versions

--- modules/StatusAuras.lua
+++ modules/StatusAuras.lua
@@ -38,10 +38,10 @@ do
 	local a, nam, tex, cnt, typ, dur, exp, cas, sid, bos, _
 	local GetAura = GetAuraDataByIndex and function(unit, index, filter) -- for retail
 		a = GetAuraDataByIndex(unit, index, filter)
-		if a then
-			fill, nam, typ, cas, sid, bos = true, a.name, a.dispelName or Bleeds[sid] and "Bleed", a.sourceUnit, a.spellId, a.isBossAura; return true; end
+		if a then fill, nam, typ, cas, sid, bos = true, a.name, a.dispelName or Bleeds[sid] and "Bleed", a.sourceUnit, a.spellId, a.isBossAura; return true; end
 	end or function(unit, index, filter) -- for classic
 		nam, tex, cnt, typ, dur, exp, cas, _, _, sid, _, bos, _, _, _, val[1], val[2], val[3] = UnitAura(unit, index, filter)
+		typ = typ or Bleeds[sid] and "Bleed"
 		if nam then if cnt==0 then cnt=1 end; return true end
 	end
 	AuraFrame_OnEvent = function(_, event, u)
