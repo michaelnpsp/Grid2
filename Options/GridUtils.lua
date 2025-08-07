f210031af2ba54930b4607f9bc46d2ae683baaa1
michaelsp <michael.npsp@gmail.com>
2025-08-07 17:22:39 +0200

    Fixing error on theme creation (GH issue #307)

--- Options/GridUtils.lua
+++ Options/GridUtils.lua
@@ -629,14 +629,14 @@ do
 
 	local function ShowDialog(message, textDefault, funcAccept, funcCancel, textAccept, textCancel)
 		local t = StaticPopupDialogs["GRID2OPTIONS_GENERAL_DIALOG"]
-		t.OnShow = function (self)	if textDefault then self.editBox:SetText(textDefault) end; self:SetFrameStrata("TOOLTIP") end
+		t.OnShow = function (self) if textDefault then (self.editBox or self:GetEditBox()):SetText(textDefault) end; self:SetFrameStrata("TOOLTIP") end
 		t.OnHide = function(self) self:SetFrameStrata("DIALOG")	end
 		t.hasEditBox = textDefault and true or nil
 		t.text = message
 		t.button1 = funcAccept and (textAccept or ACCEPT) or nil
 		t.button2 = funcCancel and (textCancel or CANCEL) or nil
 		t.OnCancel = funcCancel
-		t.OnAccept = funcAccept and function (self)	funcAccept( textDefault and self.editBox:GetText() ) end or nil
+		t.OnAccept = funcAccept and function (self) funcAccept( textDefault and (self.editBox or self:GetEditBox()):GetText() ) end or nil
 		StaticPopup_Show ("GRID2OPTIONS_GENERAL_DIALOG")
 	end
 
