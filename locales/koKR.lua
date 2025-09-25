094520c6edb93f11283fb51d4b5065c9200d4dfb
007bbb <86834094+007bbb@users.noreply.github.com>
2025-09-25 11:32:39 +0900

    Update koKR.lua

--- locales/koKR.lua
+++ locales/koKR.lua
@@ -3,7 +3,7 @@ if not L then return end
 
 --{{{ Actually used
 L["Border"] = "테두리"
-L["Charmed"] = "매혹"
+L["Charmed"] = "현혹"
 L["Default"] = "기본값"
 L["Grid2"] = "Grid2"
 L["Beast"] = "야수"
@@ -15,7 +15,7 @@ L["Elemental"] = "정령"
 --{{{ Health
 L["DEAD"] = "죽음"
 L["GHOST"] = "유령"
-L["FD"] = "죽척"
+L["FD"] = "죽은척"
 L["Offline"] = "오프라인"
 --}}}
 
@@ -25,12 +25,12 @@ L["FFA"] = "FFA"
 --}}}
 
 --{{{ Range
-L["OOR"] = "범위 벗어남"
+L["OOR"] = "범위 밖"
 --}}}
 
 --{{{ ReadyCheck
 L["?"] = "?"
-L["R"] = "R"
+L["R"] = "O"
 L["X"] = "X"
 L["AFK"] = "자리비움"
 --}}}
@@ -44,7 +44,7 @@ L["vehicle"] = "탈것"
 --}}}
 
 --{{{ VoiceComm
-L["talking"] = "대화중"
+L["talking"] = "대화 중"
 --}}}
 
 --{{{ DungeonRole
@@ -53,51 +53,85 @@ L["HEALER"] = "치유 전담"
 L["DAMAGER"] = "공격 전담"
 --}}}
 
+--{{{ SpecRole
+L["Tank"] = "탱커"
+L["Healer"] = "힐러"
+L["Melee"] = "근접"
+L["Ranged"] = "원거리"
+--}}}
+
 --{{{ Other roles
-L["RL"] = "RL"
-L["RA"] = "RA"
-L["ML"] = "ML"
+L["RL"] = "공격대장"
+L["RA"] = "부공격대장"
+L["ML"] = "전리품 담당"
 --}}}
 
 --{{{ Resurrection
-L["Reviving"] = "부활"
-L["Revived"] = "부활"
+L["Reviving"] = "부활 중"
+L["Revived"] = "부활 완료"
 ---}}}
 
 --{{{ Self
--- L["Me"] = ""
+L["Me"] = "나"
+---}}}
+
+--{{{ Combat
+L["combat"] = "전투"
 ---}}}
 
 --{{{ Summon
--- L["Pending"] = ""
--- L["Accepted"] = ""
--- L["Declined"] = ""
+L["Pending"] = "대기 중"
+L["Accepted"] = "수락함"
+L["Declined"] = "거절됨"
 ---}}}
 
 --Layouts
 L["None"] = "없음"
 L["Solo"] = "솔로잉"
-L["Solo w/Pet"] = "솔로잉, 소환수"
-L["By Group 5"] = "5인 공격대"
-L["By Group 5 w/Pets"] = "5인 공격대, 소환수"
-L["By Group 10"] = "10인 공격대"
-L["By Group 10 w/Pets"] = "10인 공격대, 소환수"
-L["By Group 15"] = "15인 공격대"
-L["By Group 15 w/Pets"] = "15인 공격대, 소환수"
-L["By Group 25"] = "25인 공격대"
-L["By Group 25 w/Pets"] = "25인 공격대, 소환수"
+L["Solo w/Pet"] = "솔로잉 (+소환수)"
+L["Party"] = "파티"
+L["Party w/Pets"] = "파티 (+소환수)"
+L["By Role"] = "역할별"
+L["By Role w/Pets"] = "역할별 (+소환수)"
+L["By Group"] = "파티별"
+L["By Group & Class"] = "파티 및 직업별"
+L["By Group & Class w/Pets"] = "파티 및 직업별 (+소환수)"
+L["By Group w/Pets"] = "파티별 (+소환수)"
+L["By Group & Role w/Pets"] = "파티 및 역할별 (+소환수)"
+L["By Group w/Tanks"] = "파티별 (탱커 포함)"
+L["By Group & Role"] = "파티 및 역할별"
 L["By Class"] = "직업별"
-L["By Class w/Pets"] = "직업별, 소환수"
-L["By Class 25"] = "25인 직업별"
-L["By Role 25"] = "25인 역할별"
-L["By Class 1 x 25 Wide"] = "1 x 25인 직업별"
-L["By Class 2 x 15 Wide"] = "2 x 15인 직업별"
-L["By Group 4 x 10 Wide"] = "4 x 10인 공격대"
-L["By Group 25 w/tanks"] = "25인 공격대, 방어 전담"
-L["By Group 40"] = "40인 공격대"
-L["By Group 25 Tanks First"] = "25인 공격대, 첫 방어 전담"
-L["By Group 10 Tanks First"] = "10인 공격대, 첫 방어 전담"
+L["By Class w/Pets"] = "직업별 (+소환수)"
+L["By Group 10"] = "10인 공격대"
+L["By Group 10 w/Pets"] = "10인 공격대 (+소환수)"
+L["By Group 15"] = "15인 공격대"
+L["By Group 15 w/Pets"] = "15인 공격대 (+소환수)"
+L["By Group 25"] = "25인 공격대"
+L["By Group 25 w/Pets"] = "25인 공격대 (+소환수)"
+L["By Class | 1x25"] = "직업별 | 1x25"
+L["By Class | 2x15"] = "직업별 | 2x15"
+L["By Group | 40"] = "공격대별 | 40"
+L["By Group | 40 w/Pets"] = "공격대별 | 40 (+소환수)"
+L["By Group | 4x10"] = "공격대별 | 4x10"
+L["By Group | 4x10 w/Pets"] = "공격대별 | 4x10 (+소환수)"
+L["By Role | 10x4"] = "역할별 | 10x4"
 L["Select Layout"] = "배치 선택"
--- L["By Group & Class"] = ""
--- L["By Group & Class w/Pets"] = ""
--- L["By Group & Role w/Pets"] = ""
+L["By Class 1 x 25 Wide"] = "직업별 1 x 25 와이드"
+L["By Class 2 x 15 Wide"] = "직업별 2 x 15 와이드"
+L["By Group 4 x 10 Wide"] = "공격대 4 x 10 와이드"
+L["By Group 25 w/tanks"] = "25인 공격대 (탱커 포함)"
+L["By Group 40"] = "40인 공격대"
+L["By Group 25 Tanks First"] = "25인 공격대 (탱커 우선)"
+L["By Group 10 Tanks First"] = "10인 공격대 (탱커 우선)"
+L["|cFFff4040Left Click|r to open configuration\n|cFFff4040Right Click|r to open layouts menu"] = "|cFFff4040좌클릭|r: 설정 열기\n|cFFff4040우클릭|r: 배치 메뉴 열기"
+L["Grid2 Visibility"] = "Grid2 표시"
+L["Grid2 Movement"] = "Grid2 이동"
+L["Profile"] = "프로필"
+L["Theme"] = "테마"
+L["Layout"] = "배치"
+L["Never"] = "표시 안 함"
+L["Raid"] = "공격대 시"
+L["Grouped"] = "파티 시"
+L["Always"] = "항상 표시"
+L["Locked"] = "잠금"
+L["debuffs"] = "디버프"
