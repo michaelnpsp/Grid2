fc14ae10c5ac981160b217d5ba80abc71b1a6a21
michaelsp <michael.npsp@gmail.com>
2026-04-11 06:12:08 +0200

    Fixing crash on private-auras indicaotr in ptr 12.0.5

--- modules/IndicatorPrivateAuras.lua
+++ modules/IndicatorPrivateAuras.lua
@@ -163,7 +163,10 @@ end
 
 Grid2.setupFunc["privateauras"] = function(indicatorKey, dbx)
 	local indicator = Grid2.indicatorPrototype:new(indicatorKey)
-	indicator.auraAnchor = { isContainer = false, iconInfo={ iconAnchor={ offsetX=0, offsetY=0, point='CENTER', relativePoint='CENTER' } } }
+	indicator.auraAnchor = { iconInfo={ iconAnchor={ offsetX=0, offsetY=0, point='CENTER', relativePoint='CENTER' } } }
+	if Grid2.versionCli>=120005 then
+		indicator.auraAnchor.isContainer = false
+	end
 	indicator.dbx       = dbx
 	indicator.Create    = Icon_Create
 	indicator.Layout    = Icon_Layout
