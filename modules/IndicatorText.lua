4adb12684c81cba56012be80dccca9a085056c2c
michaelsp <michael.npsp@gmail.com>
2026-07-08 19:16:27 +0200

    Fixing possible fontflags issue in text indicators.

--- modules/IndicatorText.lua
+++ modules/IndicatorText.lua
@@ -276,10 +276,10 @@ local function Text_UpdateDB(self)
 	self.textsize = dbx.fontSize or theme.fontSize or 11
 	if dbx.fontFlags then
 		self.shadowAlpha = dbx.shadowDisabled and 0 or 1
-		self.fontFlags   = dbx.fontFlags~='NONE' and dbx.fontFlags or nil
+		self.fontFlags   = dbx.fontFlags~='NONE' and dbx.fontFlags or ''
 	else
 		self.shadowAlpha = theme.shadowDisabled and 0 or 1
-		self.fontFlags   = theme.fontFlags~='NONE' and theme.fontFlags or nil
+		self.fontFlags   = theme.fontFlags~='NONE' and theme.fontFlags or ''
 	end
 	if Grid2.testThemeIndex then -- check layout test mode
 		self.OnUpdate = Text_OnUpdateTest
